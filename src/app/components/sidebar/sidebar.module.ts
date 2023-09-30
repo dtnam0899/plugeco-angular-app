0f1dad292399a55b1373f0d3857dafce9009f964
Harry Nam <dtnam752@gmail.com>
2023-09-30 23:55:50 +0700

    feat: routing redirect for label sidebar, install angular/cdk

--- src/app/components/sidebar/sidebar.module.ts
+++ src/app/components/sidebar/sidebar.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { SidebarComponent } from './sidebar.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [
     SidebarComponent
   ],
-  imports: [],
+  imports: [RouterModule],
   exports: [SidebarComponent]
 })
 export class SidebarModule { }
